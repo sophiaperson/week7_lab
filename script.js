1930e2642784ae489f7a090b5c0b8bf1e5ec1737
Sophia Ho <swho@andrew.cmu.edu>
2021-10-25 11:05:49 -0400

    added storage functionality

--- script.js
+++ script.js
@@ -53,13 +53,43 @@ function generateRandomAnimal() {
   }
 }
 
+function saveAnimal(animal) {
+  animalString = JSON.stringify(animal)
+  localStorage.setItem("savedAnimal", animalString)
+}
+
 function onLoad() {
-  // generate a random animal when the document opens
-  let animal = generateRandomAnimal();
+  // load saved animal if one exists
+  var animal = JSON.parse(localStorage.getItem("savedAnimal"))
+  let hasSavedAnimal = false
+  if (animal === null) {
+    document.getElementById("button-storage").textContent = "Save Animal"
+    // generate a random animal when the document opens
+    let animal = generateRandomAnimal();
+    
+  } else {
+    document.getElementById("button-storage").textContent = "Clear Animal"
+    hasSavedAnimal = true
+  }
   console.log(animal)
   // update the page based on the animal properties
   document.getElementById("animal-properties").textContent = animal.name + "  " + animal.age + "years old";
   let imageTag = document.getElementById("animal-img");
   imageTag.setAttribute("src", animal.image);
   imageTag.setAttribute("alt", animal.image_alt);
+  
+  document.getElementById("button-storage").addEventListener("click", function() {
+    if (hasSavedAnimal) {
+      localStorage.removeItem("savedAnimal")
+      document.getElementById("button-storage").style.display = "none"
+      document.getElementById("feedback-message").textContent = "Cleared!"
+      document.getElementById("feedback-message").style.display = "block"
+    } else {
+      animalString = JSON.stringify(animal)
+      localStorage.setItem("savedAnimal", animalString)
+      document.getElementById("button-storage").style.display = "none"
+      document.getElementById("feedback-message").textContent = "Saved!"
+      document.getElementById("feedback-message").style.display = "block"
+    }
+  })
 }
